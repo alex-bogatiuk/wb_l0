3c59be32d8df3ef1f32709ef50d4587660deee98
alex <gamearena0@gmail.com>
2023-07-28 23:57:41 +0300

    some tests

--- cmd/main.go
+++ cmd/main.go
@@ -1,51 +1,166 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"os"
-	"os/exec"
-	"time"
-
+	"github.com/alex-bogatiuk/wb_l0/internal/models"
+	"github.com/go-playground/validator/v10"
+	"github.com/gookit/slog"
 	"github.com/nats-io/nats.go"
+	"time"
 )
 
-func runExecutableFile(path string, args ...string) {
-	cmd := exec.Command(path, args...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	if err := cmd.Run(); err != nil {
-		fmt.Printf("Error executing %s: %v\n", path, err)
-	}
+var goodJSON = `
+{
+  "order_uid": "b563feb7b2b84b6test",
+  "track_number": "WBILMTESTTRACK",
+  "entry": "WBIL",
+  "delivery": {
+    "name": "Test Testov",
+    "phone": "+9720000000",
+    "zip": "2639809",
+    "city": "Kiryat Mozkin",
+    "address": "Ploshad Mira 15",
+    "region": "Kraiot",
+    "email": "test@gmail.com"
+  },
+  "payment": {
+    "transaction": "b563feb7b2b84b6test",
+    "request_id": "",
+    "currency": "USD",
+    "provider": "wbpay",
+    "amount": 1817,
+    "payment_dt": 1637907727,
+    "bank": "alpha",
+    "delivery_cost": 1500,
+    "goods_total": 317,
+    "custom_fee": 0
+  },
+  "items": [
+    {
+      "chrt_id": 9934930,
+      "track_number": "WBILMTESTTRACK",
+      "price": 453,
+      "rid": "ab4219087a764ae0btest",
+      "name": "Mascaras",
+      "sale": 30,
+      "size": "0",
+      "total_price": 317,
+      "nm_id": 2389212,
+      "brand": "Vivienne Sabo",
+      "status": 202
+    }
+  ],
+  "locale": "en",
+  "internal_signature": "",
+  "customer_id": "test",
+  "delivery_service": "meest",
+  "shardkey": "9",
+  "sm_id": 99,
+  "date_created": "2021-11-26T06:22:19Z",
+  "oof_shard": "1"
 }
+`
+
+var Validator = validator.New()
+
 func main() {
 
-	//run nats-server in goroutine
-	go runExecutableFile("./nats-server.exe")
+	var c models.Order
+
+	err := json.Unmarshal([]byte(goodJSON), &c)
+	if err != nil {
+		slog.Info("json UnmarshalError:", err)
+	}
+
+	err = validateStruct(c)
+	if err != nil {
+		slog.Info(err)
+	}
 
 	nc, _ := nats.Connect(nats.DefaultURL)
+	c, _ := nats.NewEncodedConn(nc, nats.JSON_ENCODER)
+	defer c.Close()
 
-	defer nc.Drain()
+	// Simple Async Subscriber
+	c.Subscribe("foo", func(s string) {
+		fmt.Printf("Received a message: %s\n", s)
+	})
 
-	nc.Publish("greet.joe", []byte("hello"))
+	// Simple Publisher
+	c.Publish("foo", "Hello World")
 
-	sub, _ := nc.SubscribeSync("greet.*")
+	// EncodedConn can Publish any raw Go type using the registered Encoder
+	type person struct {
+		Name    string
+		Address string
+		Age     int
+	}
 
-	msg, _ := sub.NextMsg(10 * time.Millisecond)
-	fmt.Println("subscribed after a publish...")
-	fmt.Printf("msg is nil? %v\n", msg == nil)
+	// Go type Subscriber
+	c.Subscribe("hello", func(p *person) {
+		fmt.Printf("Received a person: %+v\n", p)
+	})
 
-	nc.Publish("greet.joe", []byte("hello"))
-	nc.Publish("greet.pam", []byte("hello"))
+	me := &person{Name: "derek", Age: 22, Address: "140 New Montgomery Street, San Francisco, CA"}
 
-	msg, _ = sub.NextMsg(10 * time.Millisecond)
-	fmt.Printf("msg data: %q on subject %q\n", string(msg.Data), msg.Subject)
+	// Go type Publisher
+	c.Publish("hello", me)
 
-	msg, _ = sub.NextMsg(10 * time.Millisecond)
-	fmt.Printf("msg data: %q on subject %q\n", string(msg.Data), msg.Subject)
+	// Unsubscribe
+	sub, err := c.Subscribe("foo", nil)
+	// ...
+	sub.Unsubscribe()
 
-	nc.Publish("greet.bob", []byte("hello"))
+	// Requests
+	var response string
+	err = c.Request("help", "help me", &response, 10*time.Millisecond)
+	if err != nil {
+		fmt.Printf("Request failed: %v\n", err)
+	}
 
-	msg, _ = sub.NextMsg(10 * time.Millisecond)
-	fmt.Printf("msg data: %q on subject %q\n", string(msg.Data), msg.Subject)
+	// Replying
+	c.Subscribe("help", func(subj, reply string, msg string) {
+		c.Publish(reply, "I can help!")
+	})
+
+	// Close connection
+	c.Close()
+
+	/*	nc, _ := nats.Connect(nats.DefaultURL)
+
+		defer nc.Drain()
+
+		nc.Publish("greet.joe", []byte("hello"))
+
+		sub, _ := nc.SubscribeSync("greet.*")
+
+		msg, _ := sub.NextMsg(10 * time.Millisecond)
+		fmt.Println("subscribed after a publish...")
+		fmt.Printf("msg is nil? %v\n", msg == nil)
+
+		nc.Publish("greet.joe", []byte("hello"))
+		nc.Publish("greet.pam", []byte("hello"))
+
+		msg, _ = sub.NextMsg(10 * time.Millisecond)
+		fmt.Printf("msg data: %q on subject %q\n", string(msg.Data), msg.Subject)
+
+		msg, _ = sub.NextMsg(10 * time.Millisecond)
+		fmt.Printf("msg data: %q on subject %q\n", string(msg.Data), msg.Subject)
+
+		nc.Publish("greet.bob", []byte("hello"))
+
+		msg, _ = sub.NextMsg(10 * time.Millisecond)
+		fmt.Printf("msg data: %q on subject %q\n", string(msg.Data), msg.Subject)*/
+}
+
+func validateStruct(c models.Order) error {
+
+	err := Validator.Struct(c)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- internal/models/models.go
+++ internal/models/models.go
@@ -3,52 +3,52 @@ package models
 import "time"
 
 type Order struct {
-	SmID              int       `json:"sm_id"`
-	OrderUID          string    `json:"order_uid"`
-	TrackNumber       string    `json:"track_number"`
-	Entry             string    `json:"entry"`
-	Locale            string    `json:"locale"`
+	SmID              int       `json:"sm_id" validate:"required"`
+	OrderUID          string    `json:"order_uid" validate:"required"`
+	TrackNumber       string    `json:"track_number" validate:"required"`
+	Entry             string    `json:"entry" validate:"required"`
+	Locale            string    `json:"locale" validate:"required"`
 	InternalSignature string    `json:"internal_signature"`
-	CustomerID        string    `json:"customer_id"`
-	DeliveryService   string    `json:"delivery_service"`
-	Shardkey          string    `json:"shardkey"`
-	OofShard          string    `json:"oof_shard"`
-	DateCreated       time.Time `json:"date_created"`
-	Delivery          Delivery  `json:"delivery"`
-	Payment           Payment   `json:"payment"`
-	Items             []Item    `json:"items"`
+	CustomerID        string    `json:"customer_id" validate:"required"`
+	DeliveryService   string    `json:"delivery_service" validate:"required"`
+	Shardkey          string    `json:"shardkey" validate:"required"`
+	OofShard          string    `json:"oof_shard" validate:"required"`
+	DateCreated       time.Time `json:"date_created" validate:"required"`
+	Delivery          Delivery  `json:"delivery" validate:"required"`
+	Payment           Payment   `json:"payment" validate:"required"`
+	Items             []Item    `json:"items" validate:"required"`
 }
 type Delivery struct {
-	Name    string `json:"name"`
-	Phone   string `json:"phone"`
-	Zip     string `json:"zip"`
-	City    string `json:"city"`
-	Address string `json:"address"`
-	Region  string `json:"region"`
-	Email   string `json:"email"`
+	Name    string `json:"name" validate:"required"`
+	Phone   string `json:"phone" validate:"required"`
+	Zip     string `json:"zip" validate:"required"`
+	City    string `json:"city" validate:"required"`
+	Address string `json:"address" validate:"required"`
+	Region  string `json:"region" validate:"required"`
+	Email   string `json:"email" validate:"required"`
 }
 type Payment struct {
-	Amount       int    `json:"amount"`
-	PaymentDt    int    `json:"payment_dt"`
-	DeliveryCost int    `json:"delivery_cost"`
-	GoodsTotal   int    `json:"goods_total"`
+	Amount       int    `json:"amount" validate:"required"`
+	PaymentDt    int    `json:"payment_dt" validate:"required" validate:"required"`
+	DeliveryCost int    `json:"delivery_cost" validate:"required"`
+	GoodsTotal   int    `json:"goods_total" validate:"required"`
 	CustomFee    int    `json:"custom_fee"`
-	Transaction  string `json:"transaction"`
+	Transaction  string `json:"transaction" validate:"required"`
 	RequestID    string `json:"request_id"`
-	Currency     string `json:"currency"`
-	Provider     string `json:"provider"`
-	Bank         string `json:"bank"`
+	Currency     string `json:"currency" validate:"required"`
+	Provider     string `json:"provider" validate:"required"`
+	Bank         string `json:"bank" validate:"required"`
 }
 type Item struct {
-	ChrtID      int    `json:"chrt_id"`
-	Price       int    `json:"price"`
-	Sale        int    `json:"sale"`
-	TotalPrice  int    `json:"total_price"`
-	NmID        int    `json:"nm_id"`
-	Status      int    `json:"status"`
-	TrackNumber string `json:"track_number"`
-	Rid         string `json:"rid"`
-	Name        string `json:"name"`
-	Size        string `json:"size"`
-	Brand       string `json:"brand"`
+	ChrtID      int    `json:"chrt_id" validate:"required"`
+	Price       int    `json:"price" validate:"required"`
+	Sale        int    `json:"sale" validate:"required"`
+	TotalPrice  int    `json:"total_price" validate:"required"`
+	NmID        int    `json:"nm_id" validate:"required"`
+	Status      int    `json:"status" validate:"required"`
+	TrackNumber string `json:"track_number" validate:"required"`
+	Rid         string `json:"rid" validate:"required"`
+	Name        string `json:"name" validate:"required"`
+	Size        string `json:"size" validate:"required"`
+	Brand       string `json:"brand" validate:"required"`
 }
